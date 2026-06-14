8af84f3f58920cff5268ddcea170998fcc162b4e
Reema19 <reemamathew19@gmail.com>
2026-06-14 16:20:11 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,17 +20,14 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
-  subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
-  ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+  username: "Reema Mathew",
+  title: "Hi, I'm Reema Mathew",
+  subTitle:
+    "B.Tech Artificial Intelligence and Data Science student passionate about Web Development, Data Analytics, and Software Development."
 };
 
-// Social Media Links
+// Social Media github: "https://github.com/reemamathew19",
+linkedin: "https://www.linkedin.com/in/reema-mathew-b64588305",Links
 
 const socialMediaLinks = {
   github: "https://github.com/saadpasta",
